86902acadfd161f1e213d667dab1f21840d1bbb9
Ben Schwartz <benschw@gmail.com>
2015-02-02 19:11:27 -0600

    fix(parse): allow loading multiple files and wildcards

--- load_tests.go
+++ load_tests.go
@@ -11,25 +11,25 @@ var _ = fmt.Print
 func LoadTests(files []string, tests chan Test, errors chan error) {
 
 	for _, filePath := range files {
-
+		log.Println(filePath)
 		parser, err := NewTestFileParser(filePath)
 		if err != nil {
 			errors <- err
 		}
 
-		for {
-			test, err := parser.NextTest()
+		test := &Test{}
+		for test != nil {
+			test, err = parser.NextTest()
 			if err != nil {
 				errors <- err
 				return
 			}
-			if test == nil {
-				tests <- Test{Exit: true}
-				return
+			if test != nil {
+				tests <- *test
 			}
-			tests <- *test
 		}
 
 	}
+	tests <- Test{Exit: true}
 
 }
