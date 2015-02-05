55983e09271c038fa7e12b26205796a6027dd533
Ben Schwartz <benschw@gmail.com>
2015-02-05 11:40:55 -0600

    docs(gh-pages): project page

--- cli-unitw.sh
+++ cli-unitw.sh
@@ -5,13 +5,12 @@ BIN_PATH=".cli-unit/cli-unit"
 
 if [ ! -f $BIN_PATH ]; then
 	mkdir -p $(dirname $BIN_PATH)
-    curl -sS https://drone.io/github.com/benschw/cli-unit/files/cli-unit > $BIN_PATH
 			  
-    wget -qO- https://drone.io/github.com/benschw/cli-unit/files/cli-unit-`uname`-`uname -m`.gz | \
-        gunzip > $BIN_PATH
+	wget -qO- https://drone.io/github.com/benschw/cli-unit/files/cli-unit-`uname`-`uname -m`.gz | \
+		gunzip > $BIN_PATH
 
 
-    chmod +x $BIN_PATH
+	chmod +x $BIN_PATH
 fi
 
 ARGS=( "$@" )
